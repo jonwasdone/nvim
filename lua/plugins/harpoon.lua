c5778b484ffdc616eae8d56a39daf9c6a0745a42
Vasjon Done <donevasjon@gmail.com>
2024-01-21 00:39:58 +0100

    feat: remove harpoon telescope implementation

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,43 +1,20 @@
 return {
-	"ThePrimeagen/harpoon",
-	branch = "harpoon2",
-	dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
-	config = function()
-		local harpoon = require("harpoon")
-		harpoon:setup({})
+  "ThePrimeagen/harpoon",
+  branch = "harpoon2",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    local harpoon = require("harpoon")
+    harpoon:setup({})
 
-		-- basic telescope configuration
-		local conf = require("telescope.config").values
-		local function toggle_telescope(harpoon_files)
-			local file_paths = {}
-			for _, item in ipairs(harpoon_files.items) do
-				table.insert(file_paths, item.value)
-			end
+    vim.keymap.set("n", "<C-e>", function()
+      harpoon.ui:toggle_quick_menu(harpoon:list())
+    end, { desc = "Open harpoon window" })
 
-			require("telescope.pickers")
-				.new({}, {
-					prompt_title = "Harpoon",
-					finder = require("telescope.finders").new_table({
-						results = file_paths,
-					}),
-					previewer = conf.file_previewer({}),
-					sorter = conf.generic_sorter({}),
-				})
-				:find()
-		end
-
-		vim.keymap.set("n", "<leader>h", function()
-			toggle_telescope(harpoon:list())
-		end, { desc = "Open harpoon window" })
-
-		local builtin = require("telescope.builtin")
-		vim.keymap.set("n", "<leader><leader>", builtin.find_files, {})
-		vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-		vim.keymap.set("n", "<C-a>", function()
-			harpoon:list():append()
-		end)
+    vim.keymap.set("n", "<C-a>", function()
+      harpoon:list():append()
+    end, { desc = "Add current file to harpoon" })
     vim.keymap.set("n", "<C-d>", function()
       harpoon:list():remove()
-    end)
-    	end,
+    end, { desc = "Remove current file from harpoon" })
+  end,
 }
