557eab27b0a65bbee15c8a2e68cb2b874c504298
Vasjon Done <donevasjon@gmail.com>
2024-01-31 01:01:29 +0100

    feat: harpoon file navigation next/prev

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -13,8 +13,17 @@ return {
 		vim.keymap.set("n", "<C-a>", function()
 			harpoon:list():append()
 		end, { desc = "Add current file to harpoon" })
+
 		vim.keymap.set("n", "<C-x>", function()
 			harpoon:list():remove()
 		end, { desc = "Remove current file from harpoon" })
+
+		vim.keymap.set("n", "<C-h>", function()
+			harpoon:list():prev()
+		end)
+
+		vim.keymap.set("n", "<C-l>", function()
+			harpoon:list():next()
+		end)
 	end,
 }
