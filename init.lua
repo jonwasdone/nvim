c4c8a4c4257211de0a97faea111de678b9f4b1b4
Vasjon Done <donevasjon@gmail.com>
2024-06-03 08:39:06 -1000

    feat: disable swap files

--- init.lua
+++ init.lua
@@ -13,6 +13,7 @@ vim.opt.rtp:prepend(lazypath)
 vim.keymap.set("n", "<C-s>", ":update<CR>", {})
 vim.keymap.set("n", "<C-d>", "<ScrollWheelDown>", {})
 vim.keymap.set("n", "<C-u>", "<ScrollWheelUp>", {})
+vim.opt.swapfile = false
 local opts = {}
 
 require("options")
