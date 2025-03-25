b7a3891f588a405922e7f524d7ba59c1fcc5e7ce
Vasjon Done <donevasjon@gmail.com>
2025-03-25 22:33:03 +0100

    fix: warnings

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,25 +1,22 @@
 return {
-  "nvimtools/none-ls.nvim",
-  dependencies = {
-    "nvimtools/none-ls-extras.nvim",
-  },
-  config = function()
-    local null_ls = require("null-ls")
+	"nvimtools/none-ls.nvim",
+	dependencies = {
+		"nvimtools/none-ls-extras.nvim",
+	},
+	config = function()
+		local null_ls = require("null-ls")
 
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.stylua,
-        require("none-ls.code_actions.eslint").with({
-          condition = function(utils)
-            return utils.root_has_file_matches("eslintrc")
-          end,
-        }),
-        null_ls.builtins.formatting.prettier,
-        null_ls.builtins.completion.tailwindcss,
-        null_ls.builtins.formatting.fixjson,
-        null_ls.builtins.diagnostics.jsonlint,
-      },
-    })
-    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-  end,
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				require("none-ls.code_actions.eslint").with({
+					condition = function(utils)
+						return utils.root_has_file_matches("eslintrc")
+					end,
+				}),
+				null_ls.builtins.formatting.prettier,
+			},
+		})
+		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+	end,
 }
