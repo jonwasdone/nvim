859d6b3d13e2056c85516483c3ebb72c0275c373
Vasjon Done <donevasjon@gmail.com>
2025-04-05 00:43:38 +0200

    feat: small changes

--- init.lua
+++ init.lua
@@ -1,13 +1,13 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 vim.keymap.set("n", "<C-s>", ":update<CR>", {})
@@ -19,7 +19,9 @@ vim.wo.relativenumber = true
 vim.opt.nu = true
 vim.opt.relativenumber = true
 vim.o.statuscolumn = "%s %l %r "
-
+vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "white", bold = false })
+vim.api.nvim_set_hl(0, "LineNr", { fg = "#ffe5b4", bold = true })
+vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "white", bold = false })
 
 local opts = {}
 
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,28 +1,28 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-	},
-	config = function()
-		require("neo-tree").setup({
-			event_handlers = {
-				{
-					event = "file_opened",
-					handler = function(file_path)
-						require("neo-tree.command").execute({ action = "close" })
-					end,
-				},
-			},
-			filesystem = {
-				filtered_items = {
-					visible = true,
-				},
-			},
-		})
-		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
-		vim.keymap.set("n", "<C-g>", ":Neotree float git_status<CR>", {})
-	end,
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+    require("neo-tree").setup({
+      event_handlers = {
+        {
+          event = "file_opened",
+          handler = function(file_path)
+            require("neo-tree.command").execute({ action = "close" })
+          end,
+        },
+      },
+      filesystem = {
+        filtered_items = {
+          visible = true,
+        },
+      },
+    })
+    vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal float<CR>", {})
+    vim.keymap.set("n", "<C-g>", ":Neotree float git_status<CR>", {})
+  end,
 }
