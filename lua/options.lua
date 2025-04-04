f4f99408aa512fa01c7fafde4a51d904f0068c8c
Vasjon Done <donevasjon@gmail.com>
2025-04-05 01:40:55 +0200

    feat: status bar space

--- lua/options.lua
+++ lua/options.lua
@@ -3,6 +3,8 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set nu rnu")
+vim.cmd("set laststatus=3")
+vim.cmd("set cmdheight=0")
 vim.g.mapleader = " "
 
 vim.opt.number = true
