1b6a09578521e8e65bb61d2c744343af81e7ad0f
Vasjon Done <donevasjon@gmail.com>
2025-06-17 12:41:24 +0200

    feat: do

---
+++ lua/plugins/do.lua
@@ -0,0 +1,18 @@
+return {
+	"nocksock/do.nvim",
+	version = "*",
+	opts = {},
+	config = function()
+		require("do").setup({
+			-- default options
+			message_timeout = 2000, -- how long notifications are shown
+			kaomoji_mode = 0, -- 0 kaomoji everywhere, 1 skip kaomoji in doing
+			winbar = false,
+			doing_prefix = "Doing: ",
+			store = {
+				auto_create_file = false, -- automatically create a .do_tasks when calling :Do
+				file_name = ".do_tasks",
+			},
+		})
+	end,
+}
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,47 +1,61 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  opts = {
-    options = {
-      icons_enabled = true,
-      theme = "tokyonight",
-      component_separators = { left = "", right = "" },
-      section_separators = { left = "", right = "" },
-      disabled_filetypes = {
-        statusline = {},
-        winbar = {},
-      },
-      ignore_focus = {},
-      always_divide_middle = true,
-      globalstatus = false,
-      refresh = {
-        statusline = 1000,
-        tabline = 1000,
-        winbar = 1000,
-      },
-    },
-    sections = {
-      lualine_a = { "mode" },
-      lualine_b = { "branch", "diff", "diagnostics" },
-      lualine_c = { "filename" },
-      lualine_x = { "encoding", "fileformat", "filetype" },
-      lualine_y = { "progress" },
-      lualine_z = { "location" },
-    },
-    inactive_sections = {
-      lualine_a = {},
-      lualine_b = {},
-      lualine_c = { "filename" },
-      lualine_x = { "location" },
-      lualine_y = {},
-      lualine_z = {},
-    },
-    tabline = {},
-    winbar = {},
-    inactive_winbar = {},
-    extensions = {},
-  },
-  config = function(LazyPlug, opts)
-    require("lualine").setup(opts)
-  end,
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	opts = {
+		options = {
+			icons_enabled = true,
+			theme = "tokyonight",
+			component_separators = { left = "", right = "" },
+			section_separators = { left = "", right = "" },
+			disabled_filetypes = {
+				statusline = {},
+				winbar = {},
+			},
+			ignore_focus = {},
+			always_divide_middle = true,
+			globalstatus = false,
+			refresh = {
+				statusline = 1000,
+				tabline = 1000,
+				winbar = 1000,
+			},
+		},
+		sections = {
+			lualine_a = { "mode" },
+			lualine_b = { "branch", "diff", "diagnostics" },
+			lualine_c = { "filename" },
+			lualine_x = { "encoding", "fileformat", "filetype" },
+			lualine_y = { "progress" },
+			lualine_z = { "location" },
+		},
+		inactive_sections = {
+			lualine_a = {},
+			lualine_b = {},
+			lualine_c = { "filename" },
+			lualine_x = { "location" },
+			lualine_y = {},
+			lualine_z = {},
+		},
+		tabline = {},
+		winbar = {},
+		inactive_winbar = {},
+		extensions = {},
+	},
+	config = function(LazyPlug, opts)
+		require("lualine").setup(opts)
+		require("lualine").setup({
+			winbar = {
+				lualine_a = {
+					function()
+						return require("do").view("active")
+					end,
+				},
+			},
+			inactive_winbar = {
+				-- in order to prevent jumping of code in certain cursor positions this will
+				-- (at the moment) show an empty line - but only if .view has contents.
+				lualine_a = { require("do").view_inactive },
+			},
+		})
+	end,
 }
