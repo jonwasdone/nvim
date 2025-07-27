fcd9d66b5d2114f1c0809959ddcf3604ca9d8888
Vasjon Done <donevasjon@gmail.com>
2025-07-27 23:35:18 +0200

    fix: none ls

--- init.lua
+++ init.lua
@@ -1,13 +1,13 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 vim.keymap.set("n", "<C-s>", ":update<CR>", {})
@@ -26,4 +26,7 @@ vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "white", bold = false })
 local opts = {}
 
 require("options")
-require("lazy").setup("plugins")
+require("lazy").setup({
+  change_detection = { enabled = false },
+  spec = "plugins",
+})
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,23 +1,18 @@
 return {
-  "nvimtools/none-ls.nvim",
-  dependencies = {
-    "nvimtools/none-ls-extras.nvim",
-  },
-  config = function()
-    local null_ls = require("null-ls")
+	"nvimtools/none-ls.nvim",
+	dependencies = {
+		"nvimtools/none-ls-extras.nvim",
+	},
+	config = function()
+		local null_ls = require("null-ls")
 
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.stylua,
-        require("none-ls.code_actions.eslint").with({
-          condition = function(utils)
-            return utils.root_has_file_matches("eslintrc")
-          end,
-        }),
-        null_ls.builtins.formatting.prettier,
-        null_ls.builtins.formatting.black,
-      },
-    })
-    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-  end,
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.prettier,
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.diagnostics.eslint,
+			},
+		})
+		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+	end,
 }
